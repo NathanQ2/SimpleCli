a0527a63734c328fecbd0624b48d6c8b95e49fc2
Nathan Q <natethegreat08q@gmail.com>
2023-08-16 21:02:12 -0400

    more

---
+++ SimpleCLI/SimpleCli/SimpleCli.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleCli;
+
+public static class SimpleCli
+{
+    private static SimpleCliLayer? _layer;
+    public static void PushLayer(SimpleCliLayer layer)
+    {
+        _layer = layer;
+        _layer.OnPush();
+    }
+
+    public static void Start()
+    {
+        if (_layer == null)
+            return;
+        
+        _layer.OnStart();
+        while (true)
+        {
+            if (_layer.OnUpdate()) break;
+        }
+    }
+}
---
+++ SimpleCLI/SimpleCliApp/SimpleCliApp.cs
@@ -0,0 +1,53 @@
+﻿using SimpleCli;
+using SimpleCli.Arg;
+
+namespace SimpleCliApp;
+
+/// <summary>
+/// Represents the app.
+/// </summary>
+public class AppLayer : SimpleCliLayer
+{
+    /// <summary>
+    /// Command line commands passed through 
+    /// </summary>
+    private readonly Command<PotentialCommands, PotentialFlags>? _commandLineCommand;
+    public AppLayer(Command<PotentialCommands, PotentialFlags>? commandLineCommand)
+    {
+        _commandLineCommand = commandLineCommand;
+    }
+
+    /// <summary>
+    /// Gets called when this layer is pushed
+    /// </summary>
+    public override void OnPush()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Runs when SimpleCli.SimpleCli.Start() gets called
+    /// </summary>
+    public override void OnStart()
+    {
+        if (_commandLineCommand == null) return;
+        
+        Console.WriteLine($"Arg Parsed: {CommandManager.CommandEnumToString(_commandLineCommand.BaseCommand)} {CommandManager.FlagsEnumToString(_commandLineCommand.Flags)}");
+
+        if (_commandLineCommand.Flags.Contains(PotentialFlags.HelloFlag)) 
+            Console.WriteLine("Hello");
+        if (_commandLineCommand.Flags.Contains(PotentialFlags.GoodbyeFlag)) 
+            Console.WriteLine("Goodbye");
+    }
+    
+    /// <summary>
+    /// Gets called every frame
+    /// </summary>
+    /// <returns>Weather to exit application loop or not</returns>
+    public override bool OnUpdate()
+    {
+        Console.ReadLine();
+        
+        return true;
+    }
+}
